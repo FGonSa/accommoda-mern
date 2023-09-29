034ddb8a1a7b16108edb23354c0fcc1ce764e4fc
Frank Santos <fransecre@gmail.com>
2023-09-29 23:05:53 +0200

    Creadas las rutas en App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import Login from './pages/Login'
+import Registro from './pages/Registro'
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/about" element={<About/>} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Registro/>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
