f0e13d7763f19961cbdea0fee3b6ed756b401d06
Frank Santos <fransecre@gmail.com>
2023-10-08 20:58:36 +0200

    conexión con Mongoose con éxito

--- server/server.js
+++ server/server.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// busca el archivo .env en la raíz del proyecto y carga todas las variables de entorno definidas en el archivo
+dotenv.config();
 
 const app = express()
 
-app.listen(3000, () => {
-    console.log("Escuchando puerto 3000")
-})
+//Conexión con el Puerto que hay en ENV o en su defecto 6001
+const PORT = process.env.PORT || 6001;
+
+//Conexión MONGO DB
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
